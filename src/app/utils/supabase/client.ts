52d8833f7ea2ad5be94cbdd5936856e374732cdc
noescritor <luisangelhh2000@hotmail.com>
2026-09-08 00:13:02 -0600

    fix: use .supabase.com instead of .supabase.co

--- src/app/utils/supabase/client.ts
+++ src/app/utils/supabase/client.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://djwlchgkeeeqfkreebgq.supabase.co';
+// URL con .supabase.com (no .co)
+const SUPABASE_URL = 'https://djwlchgkeeeqfkreebgq.supabase.com';
 const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRqd2xjaGdrZWVlcWZrcmVlYmdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODg4MzQ5NTksImV4cCI6MjEwNDQxMDk1OX0.EvPTfH1gi6zI_W3CLWNlpV5X_9jQISC-C8hscVlBsJ4';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
